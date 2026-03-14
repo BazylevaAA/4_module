45d209cd97e9260fe0790081209276faac4a5d07
Bazyleva Anna <aab.nn16@gmail.com>
2026-03-14 15:37:46 +0300

    12

--- app/src/main/java/com/example/modul_4_pract_1_4/data/SocialRepository.kt
+++ app/src/main/java/com/example/modul_4_pract_1_4/data/SocialRepository.kt
@@ -1,6 +1,5 @@
 package com.example.modul_4_pract_1_4.data
 
-
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -10,12 +9,9 @@ import kotlinx.coroutines.withContext
 import java.io.IOException
 
 
-
-
 class SocialRepository(private val context: Context) {
     private var allPosts: List<SocialPost>? = null
     private var allComments: List<Comment>? = null
-
 
     suspend fun loadPosts(): List<SocialPost> {
         return withContext(Dispatchers.IO) {
@@ -23,7 +19,6 @@ class SocialRepository(private val context: Context) {
                 if (allPosts == null) {
                     val jsonString = context.assets.open("social_posts.json")
                         .bufferedReader().use { it.readText() }
-
 
                     val listType = object : TypeToken<List<SocialPost>>() {}.type
                     allPosts = Gson().fromJson(jsonString, listType)
@@ -36,7 +31,6 @@ class SocialRepository(private val context: Context) {
         }
     }
 
-
     suspend fun loadComments(postId: Int):List<Comment>{
         return withContext(Dispatchers.IO){
             try {
@@ -44,22 +38,15 @@ class SocialRepository(private val context: Context) {
                     val jsonString = context.assets.open("comments.json")
                         .bufferedReader().use { it.readText() }
 
-
                     val listType = object : TypeToken<List<Comment>>() {}.type
                     allComments = Gson().fromJson(jsonString, listType)
 
 
-
-
                 }
-
 
                 delay((500..1500).random().toLong())
 
-
                 allComments?.filter { it.postid == postId } ?: emptyList()
-
-
 
 
             } catch (e: IOException) {
@@ -69,18 +56,14 @@ class SocialRepository(private val context: Context) {
     }
 
 
-
-
     suspend fun loadAvatar(url: String): String {
         return withContext(Dispatchers.IO) {
             // Имитация загрузки аватарки (разное время)
             delay((300..1000).random().toLong())
 
-
             // Возвращаем цвет или URL (в реальном приложении тут была бы загрузка картинки)
             listOf("#FF6B6B", "#4ECDC4", "#45B7D1", "#96CEB4", "#FFEAA7").random()
         }
     }
-
 
 }
